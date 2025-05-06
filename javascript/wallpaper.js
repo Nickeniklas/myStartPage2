9baef429f30c37d910b167dbfbe4fbcf4110a9b5
Nickeniklas <savonhen@arcada.fi>
2025-05-06 18:00:51 +0300

    Added: more new wallpapers. Changed bg css: from cover -> fit

--- javascript/wallpaper.js
+++ javascript/wallpaper.js
@@ -6,15 +6,25 @@ console.log(window.location.origin);
 
 function randomWallpaper() {
     const images = [
-        /*"/images/barcelona-sky_view_lowRes.jpg",
-        "/images/Cafe-Wallpapers-HD-Free-download.jpg",
-        "/images/stromso_picture_collab1_lowRes.png",
-        "/images/stromso_picture_collab2_lowRes.png",
+        "/images/barcelona-sky_view_lowRes.jpg",
         "/images/inkoon_saaristo_salmi.jpg",
-        "/images/wallpaper.png"*/
+        "/images/wallpaper.png",
         "/images/arabianranta_spring_EDIT_compressed.jpg",
         "/images/kabböleterassi_sunset.jpg",
-        "/images/kim_mökki.jpg"
+        "/images/kim_mökki.jpg",
+        "/images/berlin1.jpg",
+        "/images/berlin2.jpg",
+        "/images/berlin3.jpg",
+        "/images/berlin4.jpg",
+        "/images/berlin5.jpg",
+        "/images/berlin6.jpg",
+        "/images/airplane_view.jpg",
+        "/images/airplane_view2.jpg",
+        "/images/kalasatama-sompasaari.jpg",
+        "/images/kabbole_sea.jpg",
+        "/images/arabiankirjaston-puut.jpg",
+        "/images/kabbole_sea.jpg",
+
     ];
 
     const randomIndex = Math.floor(Math.random() * images.length);
